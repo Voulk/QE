05ff3f27702ec31b54db05bc6c55ca3c7c50ec78
Voulk <itsvoulk@gmail.com>
2025-01-30 11:16:20 -0600

    Ported Deck of Cards RNG article

--- src/components/ItemLink.tsx
+++ src/components/ItemLink.tsx
@@ -5,10 +5,44 @@ type ItemLinkProps = {
 };
 
 const itemMap = {
+    // Consumables
     "Flask of Tempered Swiftness": 431972,
     "Algari Mana Oil": 224107,
     "Feast": 222732,
-    
+    "Algari Mana Potion": 212241,
+    "Tempered Potion": 212265,
+    "Crystallized Augment Rune": 224572,
+    "Dreambound Augment Rune": 211495,
+
+    // Enchants
+    "Fiery Resolve": 223778,
+    "Chant of Leeching Fangs": 223737,
+    "Crystalline Radiance": 223692,
+    "Chant of Armored Leech": 223719,
+    "Sunset Spellthread": 222893,
+    "Defender's March": 223656,
+    "Radiant Haste": 223674,
+
+    "Elusive Blasphemite": 213746,
+    "Culminating Blasphemite": 213743,
+    "Masterful Emerald": 213482, // Haste > Mastery
+    "Quick Onyx": 213494, // Mastery > Haste
+    "Quick Sapphire": 213470, // Versatility > Haste
+
+    "Spark of Omens": 211296,
+    "Binding of Binding": 215133,
+    "Darkmoon Sigil: Ascension": 226024,
+    "Darkmoon Sigil: Symbiosis": 226030,
+    "Duskthread Lining": 222873,
+    "Writhing Armor Banding": 219506,
+
+    // Cyrce's Circlet
+    "Cyrce's Circlet": 228411,
+    "Stormbringer's Runed Citrine": 228638,
+    "Fathomdweller's Runed Citrine": 228639,
+    "Legendary Skipper's Citrine": 228646,
+    "Mariner's Hallowed Citrine": 228644,
+    "Old Salt's Bardic Citrine": 228643,
 }
 
 export const checkExists = (itemName: string) => {
